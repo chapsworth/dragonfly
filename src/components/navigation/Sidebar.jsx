4e129d1ad8933576f1e89202d024206d4d6d0b08
Mike Hoss <chapsworth@gmail.com>
2025-12-31 08:51:06 +0000

    File changes

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -31,8 +31,6 @@ const iconMap = {
 const navItems = [
   { name: 'Home', page: 'Home', icon: Home },
   { name: 'Shop', page: 'Shop', icon: ShoppingBag },
-  { name: 'Strain Library', page: 'StrainLibrary', icon: Leaf },
-  { name: 'Product Library', page: 'ProductLibrary', icon: Package },
   { name: 'Glass Portal', page: 'GlassPortal', icon: Glasses },
   { name: 'Rewards', page: 'Rewards', icon: Sparkles },
   { name: 'My Orders', page: 'Orders', icon: ClipboardList },
