3a521dffbc73a972d3fc2cba7101dd756d09eac9
Mike Hoss <chapsworth@gmail.com>
2026-01-01 00:11:09 +0000

    AAA Indoor strains added

--- src/pages/VendorOrders.jsx
+++ src/pages/VendorOrders.jsx
@@ -64,8 +64,7 @@ function VendorOrdersContent() {
     { name: 'LA Bulk - AAA Indoor', type: 'flower', url: 'https://labulkflower.com/product/aaa-indoor/', defaultPrice: 300 }
   ];
 
-  const excludedVendors = ['Jared Cookie Factory', 'Pioneer', 'Factory Wholesale'];
-  const vendors = [...defaultVendors, ...vendorsData.filter(v => !defaultVendors.find(d => d.name === v.name) && !excludedVendors.includes(v.name))];
+  const vendors = [...defaultVendors, ...vendorsData.filter(v => !defaultVendors.find(d => d.name === v.name))];
 
   const { data: products = [], isLoading } = useQuery({
     queryKey: ['vendor-products', selectedVendor],
